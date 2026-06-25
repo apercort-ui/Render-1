c3aef96e673a209bd6282a7e2d8a4a51f4c580d3
apercort-ui <apercort@gmail.com>
2026-06-25 09:29:04 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,48 @@
+package main
+
 import (
-    "log"
-    "net/http"
-    "github.com/gorilla/websocket" // Теперь Go будет знать, откуда брать код
+	"log"
+	"net/http"
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+// Hub хранит активные соединения
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan []byte)
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil { log.Fatal(err) }
+	defer ws.Close()
+	clients[ws] = true
+	for {
+		_, msg, err := ws.ReadMessage()
+		if err != nil { delete(clients, ws); break }
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+		for client := range clients {
+			err := client.WriteMessage(websocket.TextMessage, msg)
+			if err != nil { client.Close(); delete(clients, client) }
+		}
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("./frontend")))
+	mux.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("./frontend/dist"))))
+	mux.HandleFunc("/ws", handleConnections)
+	go handleMessages()
+
+	log.Println("Сервер запущен на :10000")
+	http.ListenAndServe(":10000", mux)
+}
